d5208ce88be47e3c7d9f6d425b9b16f1e72d9779
0-sunshiine-0 <isabela.vieira24@escola.pr.gov.br>
2026-09-08 07:34:36 -0400

    ,,

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 let btnAjuda = document.querySelector(".botao-ajuda");
 let btnFechar = document.querySelector(".botao-ajuda");
-let modal = document.querySelector(".botao-ajuda");
+let modal = document.querySelector("modal-fundo");
+
+btnAjuda.addEventListener("click", abreModal);
+btnFechar.addEventListener("click", fechaModal);
+
+function abreModal(){
+    modal.style.display = "block";
+}
+function fechaModal(){
+    modal.style.display = "none"
+}
